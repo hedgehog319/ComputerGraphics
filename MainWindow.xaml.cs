29f31c57d1e3b5fe9b7b0f7e143d16824b415c7c
hedgehog319 <fign1910@mail.ru>
2021-03-07 15:32:51 +1000

    Init

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System.Windows;
+
+namespace ComputerGraphics
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void MenuItem_OnClick(object sender, RoutedEventArgs e)
+        {
+            // Console.WriteLine();
+        }
+
+        private void AboutProgramm_Click(object sender, RoutedEventArgs e)
+        {
+            var msg = "Просграмма DSP позволяет визуализировать сигналы с различных устройств.\n" +
+                      "Разработчики: Торжков А., Просин А., Антипов Д.";
+            MessageBox.Show(msg, "О программе");
+        }
+    }
+}
---
+++ Windows/BaseWindow.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Media;
+using Point = System.Windows.Point;
+
+namespace ComputerGraphics.Windows
+{
+    public partial class BaseWindow : UserControl
+    {
+        private bool _isMove = false;
+
+        private Point _click = new Point(0, 0);
+        private readonly TranslateTransform _transform = new TranslateTransform();
+
+        public BaseWindow()
+        {
+            InitializeComponent();
+
+            this.RenderTransform = _transform;
+        }
+
+        private void OnMouseDown(object sender, MouseButtonEventArgs e)
+        {
+            _isMove = true;
+            _click = e.GetPosition(this);
+        }
+
+        private void OnMouseUp(object sender, MouseButtonEventArgs e) => _isMove = false;
+
+        private void OnMouseLeave(object sender, MouseEventArgs e) => _isMove = false;
+
+        private void Translate(Point p)
+        {
+            var delta = _click - p;
+
+            _transform.X -= delta.X;
+            _transform.Y -= delta.Y;
+        }
+
+        private void OnMouseMove(object sender, MouseEventArgs e)
+        {
+            if (!_isMove) return;
+
+            Translate(e.GetPosition(this));
+        }
+    }
+}
