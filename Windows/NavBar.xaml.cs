b59f9088d9fde98478b792baac4c2e7c90f4441f
hedgehog319 <fign1910@mail.ru>
2021-04-02 19:23:22 +1000

    * NavBar small changes

--- Windows/NavBar.xaml.cs
+++ Windows/NavBar.xaml.cs
@@ -1,7 +1,6 @@
 ﻿using System.Windows;
 using System.Windows.Input;
 using ComputerGraphics.ChartsViews;
-using ComputerGraphics.ChartsViews.ChartView;
 using ComputerGraphics.Utils;
 
 namespace ComputerGraphics.Windows
@@ -25,15 +24,20 @@ namespace ComputerGraphics.Windows
             }
         }
 
-        private void MenuItem_OnClick(object sender, RoutedEventArgs e)
-        {
-        }
-
+        // Remove it
         private void EventSetter_OnHandler(object sender, MouseButtonEventArgs e)
         {
-            if (sender is not ChartView view) return;
-            if (view.GetModel != null)
-                new testWindow(view.GetModel) {Owner = this.Owner}.Show();
+            if (sender is not ChartView view || view.GetModel == null) return;
+
+            // new OscillogramViewer().Show();
+            // OscillogramViewer.
+        }
+
+        private void MenuItem_OnClick(object sender, RoutedEventArgs e)
+        {
+            if (sender is not ChartView view || view.GetModel == null) return;
+
+            // WindowController.InsertChart(view.GetModel);
         }
     }
 }
