ac07851ee31964903f9b017bda28a0dff736b4d0
4qwerty1 <prosin270901@gmail.com>
2021-04-24 11:36:01 +1000

    implemented classes Channel, MultiChannel

--- ComputerGraphics/Signal/Channel.cs
+++ ComputerGraphics/Signal/Channel.cs
@@ -1,7 +1,18 @@
-﻿namespace ComputerGraphics.Signal
+﻿using System.Collections.Generic;
+
+namespace ComputerGraphics.Signal
 {
     public class Channel
     {
-        
+        public string Name { get; }
+        public string Source { get; }  // источник канала
+        public List<double> Values { get; }
+
+        public Channel(string name, string source, List<double> values)
+        {
+            Name = name;
+            Source = source;
+            Values = values;
+        }
     }
 }
