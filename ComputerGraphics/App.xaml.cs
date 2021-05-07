6d9a1ae3dde1b1d7554bc04750f8d5ecde749fda
hedgehog319 <fign1910@mail.ru>
2021-05-07 16:27:09 +1000

    * Add OnStartup method

--- ComputerGraphics/App.xaml.cs
+++ ComputerGraphics/App.xaml.cs
@@ -1,13 +1,22 @@
-﻿using ComputerGraphics.Windows;
+﻿#region
+
+using System.Windows;
+using ComputerGraphics.Windows;
+
+#endregion
 
 namespace ComputerGraphics
 {
     /// <summary>
-    /// Interaction logic for App.xaml
+    ///     Interaction logic for App.xaml
     /// </summary>
     public partial class App
     {
         public App() : base()
+        {
+        }
+
+        private void App_OnStartup(object sender, StartupEventArgs e)
         {
             WindowController.ShowMainWindow();
         }
