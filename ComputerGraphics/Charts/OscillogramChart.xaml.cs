892e39911c8c810e2dcff5a888391b98445dc773
hedgehog319 <fign1910@mail.ru>
2021-05-07 18:06:44 +1000

    + OscillogramChart for view oscillogram

---
+++ ComputerGraphics/Charts/OscillogramChart.xaml.cs
@@ -0,0 +1,22 @@
+﻿#region
+
+using System;
+
+#endregion
+
+namespace ComputerGraphics.Charts
+{
+    public partial class OscillogramChart : IDisposable
+    {
+        public OscillogramChart(OscillogramModel model)
+        {
+            InitializeComponent();
+
+            DataContext = model;
+        }
+
+        public OscillogramModel GetModel => DataContext as OscillogramModel;
+
+        public void Dispose() => GetModel?.Values.Dispose();
+    }
+}
