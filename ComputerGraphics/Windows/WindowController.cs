1d175ac92d66c305b6a8982711b32d2f39abcb72
hedgehog319 <fign1910@mail.ru>
2021-05-07 18:09:29 +1000

    * No you can run some windows from here

--- ComputerGraphics/Windows/WindowController.cs
+++ ComputerGraphics/Windows/WindowController.cs
@@ -1,8 +1,20 @@
-﻿namespace ComputerGraphics.Windows
+﻿#region
+
+using ComputerGraphics.Charts;
+using ComputerGraphics.Signal;
+using Microsoft.Win32;
+
+#endregion
+
+namespace ComputerGraphics.Windows
 {
     public static class WindowController
     {
         private static readonly MainWindow MainWindow;
+        private static NavBar _navBar;
+        private static OscillogramViewer _oscillogramViewer;
+
+        private static Models _models;
 
         static WindowController()
         {
@@ -12,6 +24,38 @@
         public static void ShowMainWindow()
         {
             if (!MainWindow.IsVisible) MainWindow.Show();
+        }
+
+        public static void ShowNavBar()
+        {
+            if (_navBar != null) return;
+
+            _navBar = new NavBar(_models) {Owner = MainWindow};
+            _navBar.Closed += (s, e) => _navBar = null;
+            _navBar.Show();
+        }
+
+        public static void AddOscillogram(BaseModel model)
+        {
+            if (_oscillogramViewer == null)
+            {
+                _oscillogramViewer = new OscillogramViewer {Owner = MainWindow};
+                _oscillogramViewer.Closed += (s, e) => _oscillogramViewer = null;
+                _oscillogramViewer.Show();
+            }
+
+            // TODO Not save!
+            _oscillogramViewer.AddOscillogram(model as OscillogramModel);
+        }
+
+        public static bool ReadFile()
+        {
+            var dialog = new OpenFileDialog();
+            if (dialog.ShowDialog() != true) return false;
+
+            _models = ChannelReader.ReadTxt(dialog.FileName);
+
+            return true;
         }
     }
 }
