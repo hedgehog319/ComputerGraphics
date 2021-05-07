f0acdf0a4277225a1b3d7737b353bdf3639ee2b1
hedgehog319 <fign1910@mail.ru>
2021-05-07 18:08:42 +1000

    + PreviewChart for NavBar

---
+++ ComputerGraphics/Charts/PreviewChart.xaml.cs
@@ -0,0 +1,27 @@
+﻿#region
+
+using System;
+using System.Windows;
+using System.Windows.Controls;
+using ComputerGraphics.Windows;
+
+#endregion
+
+namespace ComputerGraphics.Charts
+{
+    public partial class PreviewChart : UserControl
+    {
+        public PreviewChart(BaseModel model)
+        {
+            InitializeComponent();
+            DataContext = model;
+        }
+
+        private void Oscillogram_OnClick(object sender, RoutedEventArgs e)
+        {
+            if (!(DataContext is BaseModel model)) throw new ArgumentException("Model is null");
+
+            WindowController.AddOscillogram(model);
+        }
+    }
+}
