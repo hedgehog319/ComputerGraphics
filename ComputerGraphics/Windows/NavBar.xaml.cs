041026f09f5ef0807ad2eb1cd7a97e97216e5e49
hedgehog319 <fign1910@mail.ru>
2021-05-07 16:24:46 +1000

    * NavBar now works!

--- ComputerGraphics/Windows/NavBar.xaml.cs
+++ ComputerGraphics/Windows/NavBar.xaml.cs
@@ -1,12 +1,19 @@
-﻿using System.Windows;
+﻿#region
+
+using System.Windows;
+using ComputerGraphics.Charts;
+
+#endregion
 
 namespace ComputerGraphics.Windows
 {
     public partial class NavBar : Window
     {
-        public NavBar()
+        public NavBar(Models models)
         {
             InitializeComponent();
+
+            foreach (var model in models.OscillogramModels) Bar.Children.Add(new PreviewChart(model) {Height = 80});
         }
     }
 }
