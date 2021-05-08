89fc50209943c6e6e09d3326b30443418253d3e0
hedgehog319 <fign1910@mail.ru>
2021-05-08 10:39:00 +1000

    + Create InfoWindow

---
+++ ComputerGraphics/Windows/InfoWindow.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows;
+
+namespace ComputerGraphics.Windows
+{
+    public partial class InfoWindow : Window
+    {
+        public InfoWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
