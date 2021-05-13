0fe111d015629c676178f99dbadd3e72071a9aab
hedgehog319 <fign1910@mail.ru>
2021-05-13 22:34:34 +1000

    + Add simulator window

---
+++ ComputerGraphics/Windows/Simulator.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System.ComponentModel;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using ComputerGraphics.Charts;
+using ComputerGraphics.Controls.ModelsUI;
+
+namespace ComputerGraphics.Windows
+{
+    public partial class Simulator : Window
+    {
+        private readonly SimulatorModel _model;
+
+        public Simulator()
+        {
+            InitializeComponent();
+            _model = DataContext as SimulatorModel;
+        }
+
+        private void UIElement_OnMouseDown(object sender, MouseButtonEventArgs e)
+        {
+            DragMove();
+        }
+
+        private void ButtonBase_OnClick(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+
+        private void Selector_OnSelected(object sender, RoutedEventArgs e)
+        {
+            if (sender is not ComboBox comboBox) return;
+
+            _model.ChangeView((SimulatorModel.Simulations) comboBox.SelectedIndex);
+            var view = _model.View as ISimulated;
+            var simulation = view?.Simulation();
+
+            //TODO change name and source
+            var oscillogramModel = new OscillogramModel("name", "Sim_Name", simulation);
+            WindowController.ChartModels.OscillogramModels.Add(oscillogramModel);
+
+            WindowController.AddOscillogram(oscillogramModel.Id);
+            Close();
+        }
+    }
+}
---
+++ ComputerGraphics/Windows/SimulatorModel.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using ComputerGraphics.Controls.ModelsUI;
+
+namespace ComputerGraphics.Windows
+{
+    public sealed class SimulatorModel : INotifyPropertyChanged
+    {
+        public enum Simulations
+        {
+            [Description("Ноль")] Zero = 0,
+            [Description("Один")] One = 1,
+        }
+
+        private object _view;
+
+        public object View
+        {
+            get => _view;
+            set
+            {
+                _view = value;
+                OnPropertyChanged(nameof(View));
+            }
+        }
+
+        public SimulatorModel()
+        {
+            View = new DelayedJump();
+        }
+
+        public void ChangeView(Simulations model)
+        {
+            View = model switch
+            {
+                Simulations.Zero => new DelayedJump(),
+                Simulations.One => new DelayedPulse(),
+                _ => throw new ArgumentOutOfRangeException(nameof(model), model, null)
+            };
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
