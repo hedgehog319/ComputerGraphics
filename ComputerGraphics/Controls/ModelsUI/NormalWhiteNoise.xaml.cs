c57795d96aa5e21d62e0c935342a5e1905feb151
4qwerty1 <prosin270901@gmail.com>
2021-05-14 22:32:23 +1000

    add NormalWhiteNoise model

---
+++ ComputerGraphics/Controls/ModelsUI/NormalWhiteNoise.xaml.cs
@@ -0,0 +1,42 @@
+using System;
+using System.Collections.Generic;
+using System.Windows.Controls;
+using System.Windows.Markup;
+using ComputerGraphics.Windows;
+
+namespace ComputerGraphics.Controls.ModelsUI
+{
+    public partial class NormalWhiteNoise : UserControl, ISimulated
+    {
+        private readonly Random _rand = new Random();
+        public NormalWhiteNoise()
+        {
+            InitializeComponent();
+        }
+
+        public List<double> Simulation()
+        {
+            if (!double.TryParse(TextMiddle.Text, out var middle) || middle < 0 ||
+                !double.TryParse(TestSigma.Text, out var sigma) || sigma < 0)
+            {
+                return null;
+            }
+            
+            var values = new List<double>();
+            for (int i = 0; i < WindowController.ChartModels.SamplesNumber; i++)
+            {
+                values.Add(middle + Math.Sqrt(sigma) * NormalRandomValue());
+            }
+
+            return values;
+        }
+
+        private double NormalRandomValue()
+        {
+            double x = 0;
+            for (int i = 0; i < 12; i++) x += _rand.NextDouble();
+
+            return x - 6;
+        }
+    }
+}
