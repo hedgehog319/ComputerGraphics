0b178c5dcd53aafb795a3107e517be4d342e4245
hedgehog319 <fign1910@mail.ru>
2021-05-14 22:14:46 +1000

    + NewSignalCreator.xaml and BestTextBox.xaml

---
+++ ComputerGraphics/Controls/BestTextBox.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Controls;
+
+namespace ComputerGraphics.Controls
+{
+    public partial class BestTextBox : UserControl
+    {
+        public BestTextBox()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ ComputerGraphics/Windows/NewSignalCreator.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Windows;
+
+namespace ComputerGraphics.Windows
+{
+    public partial class NewSignalCreator : Window
+    {
+        public int SamplesNumber { get; private set; }
+        public int SamplingRate { get; private set; }
+
+        public NewSignalCreator()
+        {
+            InitializeComponent();
+        }
+
+        private void Ok_OnClick(object sender, RoutedEventArgs e)
+        {
+            SamplesNumber = Convert.ToInt32(SamplesBox.Text);
+            SamplingRate = Convert.ToInt32(SamplingBox.Text);
+
+            this.DialogResult = true;
+            this.Close();
+        }
+
+        private void Cancel_OnClick(object sender, RoutedEventArgs e)
+        {
+            this.DialogResult = false;
+            this.Close();
+        }
+    }
+}
