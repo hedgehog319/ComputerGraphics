bf0d489620f698803a883140b3e433ffcf5fa4db
4qwerty1 <prosin270901@gmail.com>
2021-05-29 13:42:40 +1000

    for sanya

--- ComputerGraphics/Controls/ModelsUI/APCC.xaml.cs
+++ ComputerGraphics/Controls/ModelsUI/APCC.xaml.cs
@@ -2,6 +2,7 @@
 
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Windows.Controls;
 using ComputerGraphics.Windows;
 
@@ -12,7 +13,6 @@ namespace ComputerGraphics.Controls.ModelsUI
     public partial class APCC : UserControl, ISimulated
     {
         private readonly Random _rand = new Random();
-        private readonly List<double> values = new List<double>();
 
         public APCC()
         {
@@ -21,10 +21,30 @@ namespace ComputerGraphics.Controls.ModelsUI
 
         public List<double> Simulation()
         {
-            double sigma = 0;
-            for (var i = 0; i < WindowController.ChartModels.SamplesNumber; i++) values.Add(NValue(sigma));
+            try
+            {
+                double sigma = Double.Parse(Sigma.Text);
+                int p = Int32.Parse(PValue.Text);
+                int q = Int32.Parse(QValue.Text);
+                var aValues = Array.ConvertAll(ANumbers.Text.Split(' '), Double.Parse);
+                var bValues = Array.ConvertAll(BNumbers.Text.Split(' '), Double.Parse);
+                if (p != aValues.Length || q != bValues.Length)
+                    throw new Exception();
 
-            return values;
+                List<double> yValues = new List<double>();
+                List<double> xValues = new List<double>();
+                for (var i = 0; i < WindowController.ChartModels.SamplesNumber; i++)
+                {
+                    xValues.Add(NValue(sigma));
+                    yValues.Add(xValues[i] + Sum(ref bValues, xValues) - Sum(ref aValues, yValues));
+                }
+
+                return yValues;
+            }
+            catch (Exception e)
+            {
+                return null;
+            }
         }
 
         private double NValue(double sigma)
@@ -35,12 +55,13 @@ namespace ComputerGraphics.Controls.ModelsUI
             return (x - 6) * Math.Sqrt(sigma);
         }
 
-        private double QSum(List<double> B, List<double> X)
+        private double Sum(ref double[] factor, List<double> values)
         {
-            double value = 0;
-            for (var i = 0; i < B.Count && X.Count - i >= 0; i++) value += B[i] * X[X.Count - i];
+            double ans = 0;
+            for (var i = 0; i < factor.Length && values.Count - i - 1 >= 0; i++)
+                ans += factor[i] * values[values.Count - i - 1];
 
-            return value;
+            return ans;
         }
     }
 }
