e413b055d7759ed6123d29c9e8795617d6ab91b7
antipdi <danil.antipov090401@mail.ru>
2021-06-03 21:19:57 +1000

    statistics

---
+++ ComputerGraphics/Windows/Statistics.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System.Windows;
+
+namespace ComputerGraphics.Windows
+{
+    public partial class Statistics : Window
+    {
+        public Statistics()
+        {
+            InitializeComponent();
+            foreach (var model in WindowController.ChartModels)
+            {
+                box.Items.Add(model.ChannelName);
+            }
+        }
+    }
+}
