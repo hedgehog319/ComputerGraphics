87c537c1124cc7e92bd3562a06e54c4fe57367cd
hedgehog319 <fign1910@mail.ru>
2021-06-05 12:16:36 +1000

    Code cleanup

--- ComputerGraphics/Windows/Statistics.xaml.cs
+++ ComputerGraphics/Windows/Statistics.xaml.cs
@@ -1,76 +1,71 @@
-﻿using System;
+﻿#region
+
+using System;
+using System.Globalization;
 using System.Linq;
 using System.Windows;
 using System.Windows.Controls;
 using ComputerGraphics.Charts;
 
+#endregion
+
 namespace ComputerGraphics.Windows
 {
     public partial class Statistics : Window
     {
-        
-        private double _aver, _disp, _deviation, _variation, _skewness, _kurt, _min, _max;
+        private double _aver, _dispersion, _deviation;
+
         public Statistics()
         {
             InitializeComponent();
-            foreach (var model in WindowController.ChartModels)
-            {
-                box.Items.Add(model.ChannelName);
-            }
+            foreach (var model in WindowController.ChartModels) box.Items.Add(model.ChannelName);
         }
 
-        private double FuncAverage(OscillogramModel model)
+        private double FuncAverage(BaseModel model)
         {
-            return _aver = (model.Values.Sum() / model.Values.Count);
+            return _aver = model.Values.Sum() / model.Values.Count;
         }
 
-        private double FuncDispersion(OscillogramModel model)
+        private double FuncDispersion(BaseModel model)
         {
-            return _disp = (model.Values.Sum(t => Math.Pow(t - _aver, 2))) / model.Values.Count;
+            return _dispersion = model.Values.Sum(t => Math.Pow(t - _aver, 2)) / model.Values.Count;
         }
 
         private double FuncDeviation()
         {
-            return _deviation = Math.Sqrt(_disp);
+            return _deviation = Math.Sqrt(_dispersion);
         }
 
         private double FuncVariation()
         {
-            return _variation = _deviation / _aver;
+            return _deviation / _aver;
         }
 
-        private double FuncSkewness(OscillogramModel model)
+        private double FuncSkewness(BaseModel model)
         {
-            return _skewness = (model.Values.Sum(t => Math.Pow(t - _aver, 3))) / (model.Values.Count*Math.Pow(_deviation, 3));
+            return model.Values.Sum(t => Math.Pow(t - _aver, 3)) /
+                   (model.Values.Count * Math.Pow(_deviation, 3));
         }
 
-        private double FuncKurtosis(OscillogramModel model)
+        private double FuncKurtosis(BaseModel model)
         {
-            return _kurt = ((model.Values.Sum(t => Math.Pow(t - _aver, 4))) / (model.Values.Count*Math.Pow(_deviation, 4))) - 3;
+            return model.Values.Sum(t => Math.Pow(t - _aver, 4)) /
+                (model.Values.Count * Math.Pow(_deviation, 4)) - 3;
         }
 
-        private double FuncMin(OscillogramModel model)
-        {
-            return _min = model.Values.Min();
-        }
-        
-        private double FuncMax(OscillogramModel model)
-        {
-            return _max= model.Values.Max();
-        }
-        
         private void Box_OnSelectionChanged(object sender, SelectionChangedEventArgs e)
         {
             if (sender is not ComboBox comboBox) return;
             var model = WindowController.ChartModels[comboBox.SelectedIndex];
-            Average.Text = FuncAverage(model).ToString();
-            Dispersion.Text = FuncDispersion(model).ToString();
-            StandardDeviation.Text = FuncDeviation().ToString();
-            CoefficientVariation.Text = FuncVariation().ToString();
-            SkewnessCoefficient.Text = FuncSkewness(model).ToString();
-            KurtosisCoefficient.Text = FuncKurtosis(model).ToString();
-            MinimumSignalValue.Text = FuncMin(model).ToString();
-            MaximumSignalValue.Text = FuncMax(model).ToString();
+
+            Average.Text = FuncAverage(model).ToString(CultureInfo.InvariantCulture);
+            Dispersion.Text = FuncDispersion(model).ToString(CultureInfo.InvariantCulture);
+            StandardDeviation.Text = FuncDeviation().ToString(CultureInfo.InvariantCulture);
+            CoefficientVariation.Text = FuncVariation().ToString(CultureInfo.InvariantCulture);
+            SkewnessCoefficient.Text = FuncSkewness(model).ToString(CultureInfo.InvariantCulture);
+            KurtosisCoefficient.Text = FuncKurtosis(model).ToString(CultureInfo.InvariantCulture);
+            MinimumSignalValue.Text = model.Values.Min().ToString(CultureInfo.InvariantCulture);
+            MaximumSignalValue.Text = model.Values.Max().ToString(CultureInfo.InvariantCulture);
         }
     }
 }
