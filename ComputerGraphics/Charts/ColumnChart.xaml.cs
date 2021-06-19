f1984502a271bf27371034531165dbd49babf1c3
hedgehog319 <fign1910@mail.ru>
2021-06-19 12:39:41 +1000

    + ColumnChart

---
+++ ComputerGraphics/Charts/ColumnChart.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System.Windows.Controls;
+
+namespace ComputerGraphics.Charts
+{
+    public partial class ColumnChart : UserControl
+    {
+        public ColumnChart(OscillogramModel model)
+        {
+            InitializeComponent();
+
+            DataContext = model;
+        }
+
+        public OscillogramModel GetModel => DataContext as OscillogramModel;
+
+        public void Dispose() => GetModel?.Values.Dispose();
+    }
+}
